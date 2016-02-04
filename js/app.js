02650ebf9243c2384037b496d2679a481234722f
Jake Priddy <priddyjacob84@gmail.com>
2016-02-03 22:17:38 -0500

    2/3 2217

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     function playHadouken () {
-        $('#hadouken-sound')[0].volume = 0.25;
+        $('#hadouken-sound')[0].volume = 0.5;
         $('#hadouken-sound')[0].load();
         $('#hadouken-sound')[0].play(); 
     }
